2614076b45ce22b7315fb6d3a9526b1a8ae7dfd6
Pavel <oracal@mail.ru>
2018-03-26 23:24:07 +0300

    Lesson1

--- lesson1/js/script.js
+++ lesson1/js/script.js
@@ -1,2 +1,20 @@
-var budget;
-alert('Всем привет')
+var budget = +prompt("Бюжет Вашего магазина?", "");
+var nameMagazin = prompt("Название Вашего магазина?", "");
+mainList = {
+	budget,
+	nameMagazin,
+	open: "Открыто",
+		shopGoods: [tovar1 = prompt("Введите наименование товара №1", ""), tovar2 = prompt("Введите наименование товара №2", ""), tovar3 = prompt("Введите наименование товара №3", "")],
+		employers: {
+		sotr1: "Павел",
+		sotr2: "Иван",
+		sotr3: "Николай"
+				}
+};
+var budden = (budget/30);
+alert(budden);
+console.log(budget);
+console.log(nameMagazin);
+console.log(open);
+console.log(nameMagazin);
+console.log(mainList);
